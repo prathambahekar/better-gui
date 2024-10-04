395f06fa261ff2407b56dae633678412e89f8269
prathambahekar <prathambahekar97@gmail.com>
2024-10-04 10:02:27 +0530

    commit

--- main.py
+++ main.py
@@ -1,77 +1,43 @@
-# ///////////////////////////////////////////////////////////////
-#
-# BY: WANDERSON M.PIMENTA
-# PROJECT MADE WITH: Qt Designer and PySide6
-# V: 1.0.0
-#
-# This project can be used freely for all uses, as long as they maintain the
-# respective credits only in the Python scripts, any information in the visual
-# interface (GUI) can be modified without any implication.
-#
-# There are limitations on Qt licenses if you want to use your products
-# commercially, I recommend reading them on the official website:
-# https://doc.qt.io/qtforpython/licenses.html
-#
-# ///////////////////////////////////////////////////////////////
-
-# IMPORT PACKAGES AND MODULES
-# ///////////////////////////////////////////////////////////////
 from gui.uis.windows.main_window.functions_main_window import *
 import sys
 import os
 
 # IMPORT QT CORE
-# ///////////////////////////////////////////////////////////////
-from qt_core import *
 
+from qt_core import *
 # IMPORT SETTINGS
-# ///////////////////////////////////////////////////////////////
 from gui.core.json_settings import Settings
 
-# IMPORT PY ONE DARK WINDOWS
-# ///////////////////////////////////////////////////////////////
 # MAIN WINDOW
 from gui.uis.windows.main_window import *
 
-# IMPORT PY ONE DARK WIDGETS
-# ///////////////////////////////////////////////////////////////
-from gui.widgets import *
-
-# ADJUST QT FONT DPI FOR HIGHT SCALE AN 4K MONITOR
-# ///////////////////////////////////////////////////////////////
-os.environ["QT_FONT_DPI"] = "96"
-# IF IS 4K MONITOR ENABLE 'os.environ["QT_SCALE_FACTOR"] = "2"'
-
 # MAIN WINDOW
-# ///////////////////////////////////////////////////////////////
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         # SETUP MAIN WINDOw
         # Load widgets from "gui\uis\main_window\ui_main.py"
-        # ///////////////////////////////////////////////////////////////
+        
         self.ui = UI_MainWindow()
         self.ui.setup_ui(self)
 
         # LOAD SETTINGS
-        # ///////////////////////////////////////////////////////////////
+        
         settings = Settings()
         self.settings = settings.items
 
         # SETUP MAIN WINDOW
-        # ///////////////////////////////////////////////////////////////
+        
         self.hide_grips = True # Show/Hide resize grips
         SetupMainWindow.setup_gui(self)
 
-        # SHOW MAIN WINDOW
-        # ///////////////////////////////////////////////////////////////
+        # SHOW MAIN WINDOW   
         self.show()
 
-    # LEFT MENU BTN IS CLICKED
-    # Run function when btn is clicked
-    # Check funtion by object name / btn_id
-    # ///////////////////////////////////////////////////////////////
+		
+
     def btn_clicked(self):
         # GET BT CLICKED
         btn = SetupMainWindow.setup_btns(self)
@@ -85,7 +51,7 @@ class MainWindow(QMainWindow):
         top_settings.set_active(False)
 
         # LEFT MENU
-        # ///////////////////////////////////////////////////////////////
+        
         
         # HOME BTN
         if btn.objectName() == "btn_home":
@@ -161,7 +127,7 @@ class MainWindow(QMainWindow):
                 )
         
         # TITLE BAR MENU
-        # ///////////////////////////////////////////////////////////////
+        
         
         # SETTINGS TITLE BAR
         if btn.objectName() == "btn_top_settings":
@@ -187,7 +153,7 @@ class MainWindow(QMainWindow):
     # LEFT MENU BTN IS RELEASED
     # Run function when btn is released
     # Check funtion by object name / btn_id
-    # ///////////////////////////////////////////////////////////////
+    
     def btn_released(self):
         # GET BT CLICKED
         btn = SetupMainWindow.setup_btns(self)
@@ -196,27 +162,21 @@ class MainWindow(QMainWindow):
         print(f"Button {btn.objectName()}, released!")
 
     # RESIZE EVENT
-    # ///////////////////////////////////////////////////////////////
+    
     def resizeEvent(self, event):
         SetupMainWindow.resize_grips(self)
 
     # MOUSE CLICK EVENTS
-    # ///////////////////////////////////////////////////////////////
+    
     def mousePressEvent(self, event):
         # SET DRAG POS WINDOW
         self.dragPos = event.globalPos()
 
-
-# SETTINGS WHEN TO START
-# Set the initial class and also additional parameters of the "QApplication" class
-# ///////////////////////////////////////////////////////////////
 if __name__ == "__main__":
     # APPLICATION
-    # ///////////////////////////////////////////////////////////////
+    
     app = QApplication(sys.argv)
     app.setWindowIcon(QIcon("icon.ico"))
     window = MainWindow()
 
-    # EXEC APP
-    # ///////////////////////////////////////////////////////////////
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
