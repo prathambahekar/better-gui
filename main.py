2d9434c0d42da95f77663b251b8289c5e090ff29
prathambahekar <prathambahekar97@gmail.com>
2024-10-06 10:10:54 +0530

    commit

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from files.ui_functions import *
 from files.ui_main import Ui_MainWindow
 import json
 from core import *
+from win32mica import ApplyMica
+
 
 acess_settings = open("settings.json")
 Data = json.load(acess_settings) 
@@ -9,14 +11,18 @@ Data = json.load(acess_settings)
 class MainWindow(QMainWindow):
 	def __init__(self):
 		QMainWindow.__init__(self)
-
 		global defaultTheme
 
 		self.ui = Ui_MainWindow()
 		self.ui.setupUi(self)
 
 		# Applying Settings
+		ApplyMica(self)
 		UIFunctions.Setup_GUI(self)
+
+		
+		
+
 		self.show()
 
 		UIFunctions.SetTheme(self)
